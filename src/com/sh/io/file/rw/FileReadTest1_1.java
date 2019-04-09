73b5c364b21a686c3466e457b4e7f1f76d86dc57
Jioohyun <opop959551@gmail.com>
2019-04-09 11:42:08 +0900

    Update FileReadTest1_1.java

--- src/com/sh/io/file/rw/FileReadTest1_1.java
+++ src/com/sh/io/file/rw/FileReadTest1_1.java
@@ -11,11 +11,11 @@ import java.util.HashMap;
 public class FileReadTest1_1 {
 
 	public static void main(String[] args) {
-		Member m = new Member();
+		
 		File file = new File("c:\\test", "readTest.txt");
 		FileReader fr = null;
 		BufferedReader br = null;
-		ArrayList<String> ar = new ArrayList<String>(); //값을 모아준다.
+		ArrayList<Member> ar = new ArrayList<Member>(); //값을 모아준다.
 		try {
 			fr = new FileReader(file);
 			br = new BufferedReader(fr);
@@ -28,13 +28,13 @@ public class FileReadTest1_1 {
 					break;
 				}
 				String [] names = str.split("-");
-				/*
-				for(String s:names) {  //names배열에서 꺼낸 것을 String에 담자
-					System.out.println(s);
-				}*/
-				for(String s:names) {
-					ar.add(s);
-				}
+				Member member = new Member();
+				member.setGroup(names[0]);
+				member.setName(names[1]);
+				member.setAge(Integer.parseInt(names[2]));
+				member.setPart(names[3]);
+				member.setCompany(names[4]);
+				ar.add(member);
 			}
 			
 		} catch (Exception e) {
@@ -49,8 +49,13 @@ public class FileReadTest1_1 {
 			}
 		}
 		//모든 이름 출력
-		for(String s:ar) {
-			System.out.println(s);
+		for(Member s:ar) {
+			System.out.print("그룹 : "+s.getGroup());
+			System.out.print(", 이름 : "+s.getName());
+			System.out.print(", 나이 : "+s.getAge());
+			System.out.print(", 파트 : "+s.getPart());
+			System.out.print(", 회사 : "+s.getCompany());
+			System.out.println();
 		}
 	}
 
